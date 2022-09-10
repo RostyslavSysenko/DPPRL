b378f88c7bcfb13e9ba220612eab37064422b592
Rostyslav Sysenko <roxapromix@gmail.com>
2022-09-10 14:20:09 +1000

    dynamic clustering is now working, static clustering is working, median and mean  aggregations are functional and all the functionalities described prior had been tested on some basic tests

--- Clustering.py
+++ Clustering.py
@@ -3,55 +3,110 @@ import statistics
 from sklearn.neighbors import NearestNeighbors
 
 class ClusterList:
-    def __init__(self, clusterAggrFunction = "mean"):
-        self.largestCurrentIdx = 0
+    def __init__(self, cosineSimilarityThreshold = 0.9,clusterAggrFunction = "mean",indexingBitStart=None, indexingBitEnd = None ):
+        self.nextAvailIndex = 0
+        self.cosineSimilarityThreshold = cosineSimilarityThreshold
         self.clusterAggrFunction =clusterAggrFunction
         self.clusterList = []
-        self.clusterRepresentations = []; # 2d array of clusters and each cluster array contains 0/1 bit encodings
-        self.knn_classifier = NearestNeighbors(n_neighbors=1, metric="cosine")
+        self.clusterAggregations = []; # 2d array of clusters and each cluster array contains 0/1 bit encodings
+        
+        self.indexingDictionary = dict()
+        self.indexingBitStart = indexingBitStart
+        self.indexingBitEnd = indexingBitEnd
 
     def __addNewClusterToClusterList(self,clusterObj):
-        clusterObj.updateParentClusterListRef(self)
-        self.clusterRepresentations.append(clusterObj.clusterVecRepresentation)
+        # insertion
+        clusterObj.updateClusterOnClusterListInsertion(self,self.nextAvailIndex)
+        self.nextAvailIndex = self.nextAvailIndex+1
+        self.clusterAggregations.append(clusterObj.getClusterListRepresentation())
         self.clusterList.append(clusterObj)
 
-    def __growExistingClusterInAClusterList(self,rowBitString, clusterIdxToWhichWeAdd):
-        self.clusterList[clusterIdxToWhichWeAdd].addToCluster(rowBitString) #CLUSTER gets updated
-        self.clusterRepresentations[clusterIdxToWhichWeAdd] = self.clusterList[clusterIdxToWhichWeAdd].clusterRepresentation
+    def indexingHasNotBeenDoneYet(self):
+        return len(self.indexingDictionary)==0 
+
+    def __growExistingClusterInAClusterList(self,row, clusterIdxToWhichWeAdd):
+        self.clusterList[clusterIdxToWhichWeAdd].addOneRowToCluster(row) #CLUSTER gets updated
+        self.clusterAggregations[clusterIdxToWhichWeAdd] = self.clusterList[clusterIdxToWhichWeAdd].getClusterListRepresentation()
 
     def addClusterStaticly(self,clusterObj):
-        # this method is implimented for readability
-        self.__addNewClusterToClusterList(self,clusterObj)
+        # assign and increment index
+        self.__addNewClusterToClusterList(clusterObj)
 
-    def addRowDynamic(self, rowBitString):
+    def addRowDynamicNaive(self, row, indexingTurnedOn=False,indexingKey =None):
+        # this is a naive implimentation of dynamic linkage which refits the model every time a dynamic linkage is needed and then finds 1 NN based on that newly created model
         # we assume the order of clusers never changes (meaning clusters are never deleted or reordered)
-        encodingArr = [int(char) for char in rowBitString]
-
-        self.knn_classifier.fit(self.clusterRepresentations)
-        distance_mat, neighbours_vec = self.knn_classifier.kneighbors([encodingArr])
+        knn_classifier = NearestNeighbors(n_neighbors=1, metric="cosine")
+        if (not indexingTurnedOn):
+            knn_classifier.fit(self.clusterAggregations) # fit the model based on the whole data
+        else:
+            indexedClusterAggregations = self.indexingDictionary[indexingKey]
+            knn_classifier.fit(indexedClusterAggregations) #fit the model based on subset of data
+        
+        distance_mat, neighbours_vec = knn_classifier.kneighbors([row.rowListRepresentation])
         clusterIdx= neighbours_vec[0][0] # gets us index of cluster that we want to modify
         cosineSimilarity = 1 - distance_mat[0][0]
+        #print("cos sim: "+str(cosineSimilarity) + " & reccomended neighbout is at idx: " + str(clusterIdx))
 
-        if (cosineSimilarity > 0.8): # cosine similarity is high 
-            self.growExistingClusterInAClusterList(rowBitString, clusterIdx)
+        if (cosineSimilarity >= self.cosineSimilarityThreshold): # cosine similarity is high 
+            self.__growExistingClusterInAClusterList(row, clusterIdx)
         else: # cosine similarity is low 
-            idx = self.largestCurrentIdx +1
-            rowLst = [Row(rowBitString)]
-            new_cluster = Cluster(rowLst,idx)
-            self.addNewClusterToClusterList(new_cluster)
-        return clusterIdx
+            new_cluster = Cluster()
+            new_cluster.addOneRowToCluster(row)
+            self.__addNewClusterToClusterList(new_cluster)
+
+        if indexingTurnedOn:
+            self.updateIndexingDict()
+    
+    def initialIndexBuild(self):
+        # TO-DO
+        # build the indexing into the self.indexingDictionary
+        pass
+    
+    def getIndexingKey(self,row):
+        # TO-DO
+        # takes in a row and based on the indexing specification of ClusterList extracts the indexing key and returns it
+        pass
+
+    def updateIndexingDict():
+        # when row is inserted with indexing turned on, the index should update after insertion
+        pass
+
+    def addRowDynamicAdv1(self,row,indexingKey):
+        # sets up indexing dictionary, then on insertion fits knn model to the set of clusters corresponding to the index and makes prediction
+        if(self.indexingHasNotBeenDoneYet()):
+            self.initialIndexBuild()
+        
+        self.addRowDynamicNaive(row,indexing=True, indexingKey = indexingKey)
+    
+    def __str__(self) -> str:
+        returnedStr = "\n" + "largestOccupiedIndex : " + str(self.nextAvailIndex-1) +  "\n" + "cluster aggr function: " + str(self.clusterAggrFunction) + "\n" +"clusterReps: " + str(self.clusterAggregations) +"\n"+"numberOfItemsInEachCluster: ["
+
+        for cluster in self.clusterList:
+            returnedStr = returnedStr + str(cluster.getNumberOfStoredRows()) + ", "
+
+        returnedStr = returnedStr + "]"
+        return returnedStr
     
 
 class Cluster:
-    def __init__(self,id,clusterAggFunction = None):
+    def __init__(self,id = None,clusterAggFunction = None):
         self.__clusterId = id
         self.__parentClusterListObj = None
 
         self.__clusterRowObjLst = []
         self.__clusterVecAggr = []
 
+    def __clusterDoesntBelongToClustList(self):
+        return self.__parentClusterListObj == None
+
+    def getNumberOfStoredRows(self):
+        return len(self.__clusterRowObjLst)
+
     def __getAggrFunction(self):
-        return self.__parentClusterListObj.clusterAggrFunction
+        if (self.__clusterDoesntBelongToClustList()):
+            return "mean" # this is default when we create cluster without them belonging to cluster list. This is used for testing
+        else:
+            return self.__parentClusterListObj.clusterAggrFunction
 
     def __getRowNum(self):
         return len(self.__clusterRowObjLst)
@@ -61,7 +116,7 @@ class Cluster:
 
     def __setClusterAggrToMeanVector(self):
         #assume cluster has at least 1 row
-        assert len(self.clusterRowObj)>0
+        assert len(self.__clusterRowObjLst)>0
 
         meanVector = []
         rowsNum = self.__getRowNum()
@@ -82,7 +137,7 @@ class Cluster:
 
     def __setClusterAggrToMedianVector(self):
         #assume cluster has at least 1 row
-        assert len(self.clusterRowObj)>0
+        assert len(self.__clusterRowObjLst)>0
 
         medianVector = []
         
@@ -96,21 +151,40 @@ class Cluster:
         
         self.__clusterVecAggr = medianVector
 
-    def updateParentClusterListRef(self,parentClusterListObjRef):
+    def getClusterListRepresentation(self):
+        return self.__clusterVecAggr
+
+    def updateClusterOnClusterListInsertion(self,parentClusterListObjRef, clusterListClusterIdx):
         self.__parentClusterListObj = parentClusterListObjRef
+        self.__clusterId = clusterListClusterIdx
+
+        self.__handleAggregation()
 
     def getId(self):
         return self.__clusterId
 
-    def addOneToCluster(self, encodingString):
-        self.__clusterRowObjLst.append(encodingString)
+    def addOneRowToCluster(self, row):
+        self.__clusterRowObjLst.append(row) # updating the row list
+        # updating the cluster representation
+        self.__handleAggregation()
 
+    def __handleAggregation(self):
         if (self.__getAggrFunction() == "mean"):
             self.__setClusterAggrToMeanVector()
-        if (self.__getAggrFunction() == "median"):
+        elif (self.__getAggrFunction() == "median"):
             self.__setClusterAggrToMedianVector()
         else:
-            assert(False, "invalid aggregation function reached")
+            assert False, "invalid aggregation function reached: '" + self.__getAggrFunction() + "'"
+
+    def __str__(self):
+        ## this method helps to display what the cluster looks like to help with debugging
+        string = "\n" + "cluster id : " + str(self.__clusterId) + "\n" + "cluster row Objects : " 
+        
+        for row in self.__clusterRowObjLst:
+            string = string + "\n" + "    - " + str(row)
+            
+        string  = string + "\n" + "cluster agg vec : " + str(self.__clusterVecAggr) + "\n"
+        return string 
 
 class Row:
     # non modifiable currently
@@ -118,34 +192,6 @@ class Row:
         self.encodedRowString = encodedString
         self.rowListRepresentation = [int(char) for char in encodedString]
 
-
-
-
-
-
-# -----------------TESTING------------------------
-cList1 = ClusterList()
-cList2 = ClusterList()
-
-c1 = Cluster(0)
-c1.addOneToCluster(Row("010"))
-c1.addOneToCluster(Row("000"))
-c1.addOneToCluster(Row("000"))
-
-print("cluster1",c1)
-
-c2 = Cluster(1)
-c2.addOneToCluster(Row("111"))
-c2.addOneToCluster(Row("111"))
-c2.addOneToCluster(Row("110"))
-
-print("cluster2",c2)
-
-cList1.addNewCluster(c1)
-cList1.addNewCluster(c2)
-
-print("cList1 after 2x adding:",cList1)
-
-
-
+    def __str__(self):
+        return self.encodedRowString
 
---
+++ ClusteringTests.py
@@ -0,0 +1,113 @@
+from Clustering import *
+# -----------------TESTING------------------------
+
+# # TEST SET 1: some tests on mean vectors
+
+# # TEST SET 1.1: cluster class and static insertion, mean vector aggr
+# cList = ClusterList()
+
+# c0 = Cluster() # the true bit string is 1101
+# c0.addOneRowToCluster(Row("1101")) 
+# c0.addOneRowToCluster(Row("1100"))
+# c0.addOneRowToCluster(Row("1101"))
+# c0.addOneRowToCluster(Row("1101"))
+
+# print(c0)
+
+# c1 = Cluster() # the true bit string is 1111
+# c1.addOneRowToCluster(Row("1111"))
+# c1.addOneRowToCluster(Row("1111"))
+# c1.addOneRowToCluster(Row("1101"))
+# c1.addOneRowToCluster(Row("1111"))
+
+# print(c1)
+
+# c2 = Cluster() # the true bit string is 0011
+# c2.addOneRowToCluster(Row("0011"))
+# c2.addOneRowToCluster(Row("0011"))
+# c2.addOneRowToCluster(Row("0011"))
+# c2.addOneRowToCluster(Row("0011"))
+
+# cList.addClusterStaticly(c0)
+
+# print(cList)
+
+# cList.addClusterStaticly(c1)
+
+# print(cList)
+
+# cList.addClusterStaticly(c2)
+
+# print(cList)
+
+# # TEST SET 1.2: cluster class and dynamic insertion, mean vector aggr
+
+# rowForDynamicInsertion = Row("1111")
+# cList.addRowDynamicNaive(rowForDynamicInsertion)
+
+# print(cList)
+
+# rowForDynamicInsertion = Row("0011")
+# cList.addRowDynamicNaive(rowForDynamicInsertion)
+
+# print(cList)
+
+# # All tests in TEST1 are behaving apropriately on current version of code
+
+
+
+
+
+
+# # TEST SET 2 - same as test 1, just using the median vector
+
+# TEST SET 2.1 cluster class and static insertion, median vector aggr
+# cList = ClusterList(clusterAggrFunction= "median")
+
+# c0 = Cluster() # the true bit string is 1101
+# c0.addOneRowToCluster(Row("1101")) 
+# c0.addOneRowToCluster(Row("1100"))
+# c0.addOneRowToCluster(Row("1101"))
+# c0.addOneRowToCluster(Row("1101"))
+
+# print(c0)
+
+# c1 = Cluster() # the true bit string is 1111
+# c1.addOneRowToCluster(Row("1111"))
+# c1.addOneRowToCluster(Row("1111"))
+# c1.addOneRowToCluster(Row("1101"))
+# c1.addOneRowToCluster(Row("1111"))
+
+# print(c1)
+
+# c2 = Cluster() # the true bit string is 0011
+# c2.addOneRowToCluster(Row("0011"))
+# c2.addOneRowToCluster(Row("0011"))
+# c2.addOneRowToCluster(Row("0011"))
+# c2.addOneRowToCluster(Row("0011"))
+
+# cList.addClusterStaticly(c0)
+
+# print(cList)
+
+# cList.addClusterStaticly(c1)
+
+# print(cList)
+
+# cList.addClusterStaticly(c2)
+
+# print(cList)
+
+# # # TEST SET 2.2: cluster class and dynamic insertion, median vector aggr
+
+# rowForDynamicInsertion = Row("1111")
+# cList.addRowDynamicNaive(rowForDynamicInsertion)
+
+# print(cList)
+
+# rowForDynamicInsertion = Row("0011")
+# cList.addRowDynamicNaive(rowForDynamicInsertion)
+
+# print(cList)
+
+# # # # All tests in TEST2 are behaving apropriately on current version of code
