51ad4fdb32df659be57264c6b5596bc579a5b208
harrys522 <harry.sackman@students.mq.edu.au>
2022-09-16 16:56:33 +1000

    INSERT new record with delimited attributes

--- ClientEncoder.py
+++ ClientEncoder.py
@@ -4,56 +4,69 @@ from mimetypes import init
 from BloomFilter import *;
 from enum import Enum
 from bitarray import bitarray
+import socket
+
 
 class FieldType(Enum):
     INT = 1
     STR = 2
 
+
 class FileEncoder:
-    def __init__(self,attributeTypesList, fileLocation):
+    def __init__(self, attributeTypesList, fileLocation):
         self.attributeTypesList = attributeTypesList
         self.fileLocation = fileLocation
         self.bf = bf
-        self.encoding = None
+        self.encodings = None
 
     def encodeByAttribute(self):
         recordDict = bf.__read_csv_file__(self.fileLocation, True, 0)
         allEncodings = []
-        
+
         for rec in recordDict:
             encodedRecord = ""
-            encodedAttributesOfRow = bitarray()
-            
-            #this for loop goes over the row and and encodes each attributes individually
-            for attributeIdx in range(0,len(self.attributeTypesList)):
+            encodedAttributesOfRow = []
+
+            # Populate encoded attributes of row array using the INT/STR datatype key
+            for attributeIdx in range(0, len(self.attributeTypesList)):
                 currentAttribute = recordDict[rec][attributeIdx]
                 encodedAttribute = None
-                
-                if(self.attributeTypesList[attributeIdx] == FieldType.INT):                    
+
+                if (self.attributeTypesList[attributeIdx] == FieldType.INT):
                     numerical = int(currentAttribute)
-                    intValueSet1, intValueSet2 = bf.convert_num_val_to_set(numerical, 0) # This part may be unfinished
+                    intValueSet1, intValueSet2 = bf.convert_num_val_to_set(numerical, 0)  # This part may be unfinished
                     encodedAttribute = bf.set_to_bloom_filter(intValueSet1)
-                if(self.attributeTypesList[attributeIdx] == FieldType.STR):
-                    encodedAttribute = bf.set_to_bloom_filter(currentAttribute)                
+                if (self.attributeTypesList[attributeIdx] == FieldType.STR):
+                    encodedAttribute = bf.set_to_bloom_filter(currentAttribute)
 
                 assert encodedAttribute != None, encodedAttribute
-                encodedAttributesOfRow.extend(encodedAttribute)
+                encodedAttributesOfRow.append(str(encodedAttribute))
+                # encodedAttributesOfRow.extend(encodedAttribute)
 
             # Concatenate encoded attributes into a single encoding string
-            encodedRecord = "".join(str(encodedAttributesOfRow))
+            # encodedRecord = "".join(str(encodedAttributesOfRow))
 
-            # add the encoding string of the row to the list of all encoded rows
-            allEncodings.append(str(encodedRecord))
-        self.encoding = allEncodings
+            # Delimit encoded attributes with a comma into a single string
+            for i in encodedAttributesOfRow:
+                encodedAttr = i.strip("bitarray('')")
+                print(encodedAttr)
+                encodedRecord += encodedAttr + ","
 
-    def display(self,headRowNumber):
+            # add the encoded string of the row to the list of all encoded rows
+            allEncodings.append(encodedRecord)
+
+        self.encodings = allEncodings
+
+    def display(self, headRowNumber):
         # headRowNumber is the number of rows starting from the top
-        for i in range(0,headRowNumber):
-            print(self.encoding[i])
+        for i in range(0, headRowNumber):
+            print(self.encodings[i])
+
 
 # Bloom filter configuration settings
-bf_len = 50 #50
-bf_num_hash_func = 2 #2
+# Extra functionality: Move to a separate configuration file
+bf_len = 50  # 50
+bf_num_hash_func = 2  # 2
 bf_num_inter = 5
 bf_step = 1
 max_abs_diff = 20
@@ -62,16 +75,57 @@ max_val = 100
 q = 2
 
 bf = BF(bf_len, bf_num_hash_func, bf_num_inter, bf_step,
-          max_abs_diff, min_val, max_val, q)
+        max_abs_diff, min_val, max_val, q)
 
+# Client encoding script
+ipv4 = socket.AF_INET
+tcp = socket.SOCK_STREAM
+host = '127.0.0.1'
+port = 43555
 
-"""
-attributeTypesList = [FieldType.INT,FieldType.STR,FieldType.STR,FieldType.STR,FieldType.INT]
+s = socket.socket(ipv4, tcp)
+print("Client socket successfully created")
+
+# connecting to the server
+s.connect((host, port))
+print("the socket has successfully connected to server")
+# receive data from the server and decode to get the string.
+print(s.recv(1024).decode())
+# Ask server to authenticate and assign a client ID.
+
+s.send('AUTH'.encode())
+rcvd = s.recv(1024).decode()
+id = rcvd
+
+print("Client ID is ", id)
+
+# File parameters
+attributeTypesList = [FieldType.INT, FieldType.STR, FieldType.STR, FieldType.STR, FieldType.INT] # Test this key with all string types.
 fileLocation = './datasets_synthetic/ncvr_numrec_5000_modrec_2_ocp_0_myp_0_nump_5.csv'
 
-fileEncoder = FileEncoder(attributeTypesList,fileLocation)
+# Encode the csv using bloom filters
+Encoder = FileEncoder(attributeTypesList, fileLocation)
+Encoder.encodeByAttribute()
+# Extra functionality: Offline mode, do this without connecting to server and output to csv
+print("Sample of encoded data:")
+Encoder.display(5)
 
-fileEncoder.encodeByAttribute()
 
-"""
+# Send the encodings for static linkage
+print("Sending encoded data")
+for r in Encoder.encodings:
+    cmd = "STATIC INSERT " + str(r)
+    s.send(cmd.encode())
+    AcknowledgedReceive = False
+    while True:
+        rcvd = s.recv(1024).decode()
+        if rcvd.startswith("ACK"):
+            AcknowledgedReceive = True
+        if AcknowledgedReceive:
+            break
+    # Continue to next record once acknowledged
+#s.send('LIST'.encode())
 
+# Quit
+s.send('QUIT'.encode())
+s.close()
--- server.py
+++ server.py
@@ -1,7 +1,29 @@
-import socket;
-import pickle;
+import socket
+import pickle
+import Clustering
+
+
+# bit per attribute
+#  -> if static
+# 	-> pass list of rows like ["010010","01100101"] for each dataset
+#   -> "static"
+#   -> operation is insert only else return to client error
+# -> if dynamic
+#   -> "dynamic"
+# 	-> operation {insert,modify,delete}
+# 		-> if insert/delete pass a list of single row
+# 		-> if modification then pass list of 2 rows (old and new)
+# in short on each iteration of communication we need to pass following
+#
+# 1. list of row encodings (that list will represent different thing based on description above)
+# 2. type of linkage "static"/"dynamic"
+# 3. operation to be done
 
 class Server:
+    def __init__(self):
+        self.clusterlist = Clustering.ClusterList()
+        self.database1 = [] # List of all bloom filters from first database.
+
     def setUpSocketOnCurrentMachine(self):
         host = ''
         port = 43555
@@ -11,15 +33,15 @@ class Server:
         server_socket = socket.socket(ipv4, tcp)
         print("Socket successfully created")
 
-        server_socket.bind((host, port)) # bind socket to a port
+        server_socket.bind((host, port))  # bind socket to a port
         print("socket binded to %s" % (port))
 
-        server_socket.listen(5) # put the socket into listening mode
+        server_socket.listen(5)  # put the socket into listening mode
         print("socket is listening")
 
         return server_socket
 
-    def clientSend(c,message):
+    def clientSend(c, message):
         encoded = message.encode()
         c.send(encoded)
 
@@ -27,11 +49,11 @@ class Server:
         rMessage = connection.recv(1024)
         return rMessage.decode()
 
-    def receive(c,buffer):
+    def receive(c, buffer):
         rMessage = c.recv(buffer)
         return rMessage.decode()
 
-    def launchServer(self,server_socket):
+    def launchServer(self, server_socket):
         # a forever loop until we interrupt it or an error occurs
         while True:
             # Establish connection with client.
@@ -39,12 +61,12 @@ class Server:
             print('Got connection from', client_addr)
 
             # Notify client of successful connection
-            Server.clientSend(client_socket,'Connection successful')
+            Server.clientSend(client_socket, 'Connection successful')
 
             # addressing all the queries posed by a client to which we connected
             while True:
                 # Loop to receive client messages
-                rcvd = Server.receive(client_socket,1024)
+                rcvd = Server.receive(client_socket, 1024)
 
                 if not rcvd:
                     continue
@@ -52,34 +74,30 @@ class Server:
 
                 # Authenticate new client (example function)
                 if rcvd == 'AUTH':
-                    Server.clientSend(client_socket,"1") # Tell the client to use id = 1
-                    # Extra functionality (low priority): connection handling for multiple clients in real time
+                    Server.clientSend(client_socket, "1")  # Tell the client to use id = 1
+                    # Extra functionality (low priority): connection handling for multiple clients
 
-                if rcvd.startswith("DATA"):
-                    data = rcvd.split(" ")
-                    print("RECIEVED: ", data)
+                if rcvd.startswith("STATIC INSERT"):
+                    # Receive encoding
+                    splitRcvd = rcvd.split(" ")
+                    rec = splitRcvd[2]
+                    #rec = rec.strip("bitarray('')")
+                    print(rec)
+                    newRecord = Clustering.Row(rec)
+                    self.database1.append(newRecord)
+                    """            
+                    newCluster = Cluster()
+                    newCluster.addOneRowToCluster(newRecord)
+                    self.clusters.addClusterStaticly(newCluster)
+                    #self.database1.append(Row(splitRcvd[2]))
+                    """
+                    Server.clientSend(client_socket,"ACK")
 
-                if rcvd.startswith("ENCODINGS"):
-                    # Identify the size of data being received/sent
+                    # Close the connection with the client
+                if rcvd.startswith("LIST"):
+                    for i in self.database1:
+                        print(i)
 
-                    # Receive encodings
-                    pickledEncodings = str(rcvd) # String type conversion is a temporary fix, output not correct!
-                    while True:
-                        # Receive until no more messages to receive
-                        rcvd = client_socket.recv(4096)
-                        if not rcvd:
-                            break
-                        pickledEncodings += str(rcvd)
-
-                    #pickledEncodings = dataRcvd
-                    #Encodings = pickle.loads(pickledEncodings) # Fails here
-                    print(pickledEncodings)
-
-
-
-                # if new condition
-
-                # Close the connection with the client
                 if rcvd == 'QUIT':
                     client_socket.close()
                     # Breaking the loop once connection is closed
@@ -90,6 +108,7 @@ def main():
     server = Server()
     server_socket = server.setUpSocketOnCurrentMachine()
     server.launchServer(server_socket)
+
 
 if __name__ == "__main__":
     main()
