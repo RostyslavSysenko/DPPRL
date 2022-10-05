f28f02ab44d72e6eff2f3b4b20d69f50a17c2f20
Rostyslav Sysenko <roxapromix@gmail.com>
2022-10-05 18:56:26 +1100

    added some comments to code to help team navigate our code base

--- Server_program/data_structures/Indexer.py
+++ Server_program/data_structures/Indexer.py
@@ -1,4 +1,6 @@
 class Indexer:
+
+
     def __init__(self,indexingBitStart=None, indexingBitEnd = None):
         self.indexingDictionary = dict() # used for indexing
         self.indexingBitStart = indexingBitStart
@@ -7,16 +9,41 @@ class Indexer:
     def indexingHasNotBeenDoneYet(self):
         return len(self.indexingDictionary) == 0
 
-    def initialIndexBuild(self):
+    def initialIndexBuild(self, rowList): # [Row(),Row()..]
+        # input: list of Row objects 
+        # out: build up self.indexingDictionary 
+
+        # list 
+
+        #index = index.encodedStr[3:100]
+        #index = index.nonEncodedAttrubuteDict dictionati["name"] -> "josh"
+        
         # TO-DO
         # build the indexing into the self.indexingDictionary
         pass
     
     def getIndexingKey(self,row):
+        # INPUT : row object
+        # outout: some sort of key (maybe string or number)
         # TO-DO
         # takes in a row and based on the indexing specification of ClusterList extracts the indexing key and returns it
         pass
 
-    def updateIndexingDict():
-        # when row is inserted with indexing turned on, the index should update after insertion
-        pass
+        # step 2:
+        # def updateIndexingDict():
+        #     # when row is inserted with indexing turned on, the index should update after insertion
+        #     pass
+
+        # step2: def multistage indexing 
+
+
+
+indexerObject = Indexer()
+
+indexerObject.initialIndexBuild(listRows)
+
+# row encoding = "0101 0010 0101" and we want the key of that encoding to be first 4 bits
+# suppose we choose first 4 bits for indexing then indexing key = "0101"
+
+# indexerObject.indexingDictionary[indexingKey] -> relevant staff (relevant rows)
+
