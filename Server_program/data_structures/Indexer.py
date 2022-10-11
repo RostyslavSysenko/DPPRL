7e20fbcbeb37abcd7a1082226adfaa9934add19b
msobbi <majied.sobbi@students.mq.edu.au>
2022-10-12 01:16:51 +1100

    index

--- Server_program/data_structures/Indexer.py
+++ Server_program/data_structures/Indexer.py
@@ -1,3 +1,8 @@
+from enum import Enum
+import statistics
+import json
+
+
 class Indexer:
 
 
@@ -9,20 +14,52 @@ class Indexer:
     def indexingHasNotBeenDoneYet(self):
         return len(self.indexingDictionary) == 0
 
-    def initialIndexBuild(self, rowList): # [Row(),Row()..]
+
+class Row:
+    # non modifiable currently
+    def __init__(self,encodedStr: str, nonEncodedAttrubuteDict =None, rowId = None, DbId = None):
+        self.encodedRowString = encodedStr
+        self.rowListRepresentation = [int(char) for char in encodedStr]
+        self.nonEncodedAttrubuteDict =nonEncodedAttrubuteDict
+
+        self.rowId =rowId
+        self.DbId=DbId
+
+        self.clusterRef = None # this will be updated during cluster assignment
+# parse jsonStr into dictionary object:
+        jsonObj = json.loads(jsonStr)
+
+        # creating encoded string
+        lstOfEncodings = list(jsonObj["encodedAttributes"].values())
+        encodedStr =   ''.join(lstOfEncodings) #concat all encodings in a list
+
+
+        row = Row(
+                DbId= jsonObj["DBId"],
+                rowId = jsonObj["rowId"],
+                encodedStr = encodedStr,
+                nonEncodedAttrubuteDict= jsonObj["nonEncodedAttributes"]
+                )
+        return row
+
+    
+    def initialIndexBuild(self, Row): # [Row(),Row()..]
         # input: list of Row objects 
-        # out: build up self.indexingDictionary 
+        # out: build up self.indexingDictionary
 
-        index = index.encodedStr[3:20]
-
-        print(index) #need to add more in the indexing to connect it to the indexing 
+        row_build = {DbId,rowId}
 
 
+        values = row_build.values
+        values_list = list(values)
 
-        #or 
 
-        
-        encodedStr_at_index = list(dic.encodedStr())[index]
+        value = values_list[0]
+
+
+        print (value)
+
+      
 
         # list 
 
@@ -31,25 +68,43 @@ class Indexer:
         
         # TO-DO
         # build the indexing into the self.indexingDictionary
-        pass
+        
     
-    def getIndexingKey(self,row):
+    def getIndexingKey(self,Row):
         # INPUT : row object
         # outout: some sort of key (maybe string or number)
         # TO-DO
         # takes in a row and based on the indexing specification of ClusterList extracts the indexing key and returns it
 
-        search_key = '0101'
+        row_build = {DbId,rowId}
 
-        res = list(encodedStr.keys()).index(search_key) #cant figure this one out, but very similar to this
+        keys_list  = list(row_build)
+        
+        key = keys_list[0]
 
-        print(str(res))
+        print(key)
 
 
 
 
 
-        pass
+        
+
+
+
+
+
+        #search_key = '0101'
+
+        #res = list(encodedStr.keys()).index(search_key) #cant figure this one out, but very similar to this
+
+        #print(str(res))
+
+
+
+
+
+        
 
         # step 2:
         # def updateIndexingDict():
@@ -62,7 +117,7 @@ class Indexer:
 
 indexerObject = Indexer()
 
-indexerObject.initialIndexBuild(listRows)
+#indexerObject.initialIndexBuild(listRows)
 
 # row encoding = "0101 0010 0101" and we want the key of that encoding to be first 4 bits
 # suppose we choose first 4 bits for indexing then indexing key = "0101"
